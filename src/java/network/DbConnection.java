b8ef8a44da0cffbd34ee9a5e80f55988a12d96c1
Dinesh kothalanka <135213921+Dineshkothalanka@users.noreply.github.com>
2025-05-11 16:36:46 +0530

    Update DbConnection.java

--- src/java/network/DbConnection.java
+++ src/java/network/DbConnection.java
@@ -1,32 +1,30 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package network;
 
-/**
- *
- * @author java3
- */
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
-/**
- *
- * @suresh java3
- */
 public class DbConnection {
-    public static Connection getConnection()
-    {
+
+    public static Connection getConnection() {
         Connection con = null;
-        try{
-             Class.forName("com.mysql.jdbc.Driver");
-             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lsdss", "root", "dinesh");
-        }
-        catch(Exception e)
-        {
+
+        try {
+            String host = System.getenv("DB_HOST");
+            String db = System.getenv("DB_NAME");
+            String user = System.getenv("DB_USER");
+            String pass = System.getenv("DB_PASS");
+
+            String url = "jdbc:mysql://" + host + ":3306/" + db + "?useSSL=false&allowPublicKeyRetrieval=true";
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, pass);
+
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
+
         return con;
     }
 }
+
